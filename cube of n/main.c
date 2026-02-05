385d6852b137309e9ce0e47cb731577a7aa7a3cb
Vedant-01 <vedantambigar1@gmail.com>
2026-02-05 22:19:40 +0530

    Implemented cube of n logic

---
+++ cube of n/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n , cube;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    cube = n * n * n;
+
+    printf("cube is : %d", cube);
+    return 0;
+}
